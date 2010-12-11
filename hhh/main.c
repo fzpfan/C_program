fb30c201f80a365d63df93c594dab4f2917d9679
xiaopei <xiaopei@ubuntu.ubuntu-domain>
2010-12-11 11:43:45 +0800

    ccccccccccccc语言

---
+++ hhh/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int i=97;
+    printf("sizeof int is %d\n",sizeof(int));
+    printf("sizeof char is %d\n",sizeof(char));
+    printf("sizeof double is %d\n",sizeof(double));
+    printf("sizeof float is %d\n",sizeof(float));
+    printf("sizeof long is %d\n",sizeof(long));
+    printf("%d\n",i);
+    printf("%x\n",i);
+    return 0;
+}
+
+    
