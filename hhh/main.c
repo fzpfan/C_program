f28134c03e5d76d4a98770defb558f34303521ab
xiaopei <xiaopei@ubuntu.ubuntu-domain>
2010-12-13 13:30:10 +0800

    wode biji

--- hhh/main.c
+++ hhh/main.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
-int main()
-{
-    int i=97;
-    printf("sizeof int is %d\n",sizeof(int));
-    printf("sizeof char is %d\n",sizeof(char));
-    printf("sizeof double is %d\n",sizeof(double));
-    printf("sizeof float is %d\n",sizeof(float));
-    printf("sizeof long is %d\n",sizeof(long));
-    printf("%d\n",i);
-    printf("%x\n",i);
+
+int main(void)
+{ 
+    int i;
+    char a[5];
+    a[0]='a';
+    a[1]='b';
+    a[2]='c';
+    for(i=0;i<3;i++)
+    {
+        putchar(a[i]);
+        putchar('\n');
+    }
     return 0;
 }
 
-    
